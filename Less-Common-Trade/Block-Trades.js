631790a1845883bcfcf33c252f9d9b171a745568
M. Reza Zerehpoosh <ironcladgeek@hotmail.com>
2019-05-11 00:44:42 +0430

    Filter to find Block-Traded stocks

---
+++ Less-Common-Trade/Block-Trades.js
@@ -0,0 +1,10 @@
+/** 
+ * Duty: Find block traded stocks
+ * Usable: Every day of the week
+ * 
+ * Author: M. Reza Zerehpoosh
+ * Email: ironcladgeek@hotmail.com
+ */
+
+// Find the tickers that ends with '2' which had more than 10M trading volume
+((l18)[(l18).length - 1] == '2') && ((tvol) > 10000000)
